cad916024d082b7881b2c4fae0aa1ef0b2a332eb
Grisu118 <grisu118@grisu118.ch>
2016-03-14 12:02:42 +0100

    Size Controller

---
+++ app/Http/Controllers/SizeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Size;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SizeController extends Controller
+{
+    public function index()
+    {
+        $sizes = Size::all();
+        return \Response::json([
+            'data' => $this->transformCollection($sizes)
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $size = Size::find($id);
+
+        if (!$size) {
+            return \Response::json([
+                'error' => [
+                    'message' => 'Size does not exist'
+                ]
+            ], 404);
+        }
+
+        return \Response::json([
+            'data' => $this->transform($size)
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+
+        if (!$request->text or !$request->price) {
+            return \Response::json([
+                'error' => [
+                    'message' => 'Please Provide Both text and price'
+                ]
+            ], 422);
+        }
+        $size = Size::create($request->all());
+
+        return \Response::json([
+            'message' => 'Size Created Succesfully',
+            'data' => $this->transform($size)
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        if (!$request->text or !$request->price) {
+            return \Response::json([
+                'error' => [
+                    'message' => 'Please Provide Both text and price'
+                ]
+            ], 422);
+        }
+
+        $size = Size::find($id);
+        $size->text = $request->text;
+        $size->price = $request->price;
+        $size->save();
+
+        return \Response::json([
+            'message' => 'Size Updated Succesfully'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        Size::destroy($id);
+    }
+
+    private function transformCollection($sizes)
+    {
+        return array_map([$this, 'transform'], $sizes->toArray());
+    }
+
+    private function transform($size)
+    {
+        return [
+            'id' => $size['id'],
+            'text' => $size['text'],
+            'price' => $size['price']
+        ];
+    }
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['prefix' => 'api'], function(){
+    Route::resource('sizes', 'SizeController');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
