1bd5b92f4e62f934a6f91697cce3c9a4a267830f
Grisu118 <grisu118@grisu118.ch>
2016-03-20 12:51:06 +0100

    bower dependencies

--- resources/views/adm/index.blade.php
+++ resources/views/adm/index.blade.php
@@ -5,6 +5,15 @@
     <title>Image Shop</title>
 
     <script type="text/javascript" src="{{ asset('bower_components/angular/angular.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/angular-ui-router/release/angular-ui-router.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/satellizer/satellizer.js') }}"></script>
+
+    <script type="text/javascript" src="{{ asset('bower_components/angular-bootstrap/ui-bootstrap-tpls.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/bootstrap/dist/js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('bower_components/jquery/dist/jquery.js') }}"></script>
+
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('bower_components/components-font-awesome/css/font-awesome.css')}}">
 </head>
 <body>
 Welcome to the backend!
