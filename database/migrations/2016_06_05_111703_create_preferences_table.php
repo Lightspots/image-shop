24e24c80165d4911dc8b225d4a492796439008ec
Grisu118 <grisu118@grisu118.ch>
2016-06-05 13:56:18 +0200

    Preferences and Shipping Costs in Backend

---
+++ database/migrations/2016_06_05_111703_create_preferences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->string('key')->unique();
+            $table->primary('key');
+            $table->string('value');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('preferences');
+    }
+}
