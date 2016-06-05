81ffbe2263f7939cd1288ec28739a932d37f3bc7
Grisu118 <grisu118@grisu118.ch>
2016-06-05 13:15:37 +0200

    Add DomPdf

--- config/app.php
+++ config/app.php
@@ -158,6 +158,8 @@ return [
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
+        Barryvdh\DomPDF\ServiceProvider::class,
+
     ],
 
     /*
@@ -206,6 +208,8 @@ return [
 
         'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
